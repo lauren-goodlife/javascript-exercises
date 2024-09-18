4272bf3fb268026e968fdc152c7684dfa5348914
Lorena <106349039+lauren-goodlife@users.noreply.github.com>
2024-09-18 23:47:10 +0200

    Resolved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
+function repeatString(string, numRepetition) {
 
-};
+    var newString = '';
+
+
+    if (numRepetition < 0) {
+        return 'ERROR';
+    } 
+    
+    else if (numRepetition === 0) {
+        return '';
+    }   
+    
+    else {
+      for (let i = 0; i < numRepetition; i++) {
+            newString += string;
+        }
+    return newString;
+    }         
+    
+}
+
 
 // Do not edit below this line
 module.exports = repeatString;
