3f3ca0a03398daf0ad49e43232d62359bcb5f76a
Lorena <106349039+lauren-goodlife@users.noreply.github.com>
2024-09-19 00:08:18 +0200

    Resolved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+function reverseString(string) {
 
-};
+    var splittedString = string.split("");
+    var reverseString = ''
+
+    for (let i = (splittedString.length - 1); i >= 0; i--) {
+        reverseString += splittedString[i];
+    }
+
+    return reverseString;              
+
+}
+
+console.log(reverseString(''))
 
 // Do not edit below this line
 module.exports = reverseString;
