808987de763781c44868d5f1a9ee620da5358f86
Lorena <106349039+lauren-goodlife@users.noreply.github.com>
2024-09-26 23:21:58 +0200

    Exercise resolved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,38 @@
-const sumAll = function() {
+function sumAll(valoreInizio,valoreFine){
 
-};
+
+    if ((Number.isInteger(valoreInizio)) && (Number.isInteger(valoreFine)))
+        
+        {
+    
+            if (valoreInizio > 0 && valoreFine > 0) {
+                
+                let sum = 0;
+        
+                for (let i = Math.min(valoreInizio,valoreFine); i <= Math.max(valoreInizio,valoreFine); i++)
+                    
+                {
+        
+                sum += i;
+        
+                }
+        
+                return sum;
+        
+            } else {
+    
+                return 'ERROR';
+            
+                }
+    
+        } else {
+
+            return 'ERROR';
+        }
+        
+    } 
+    
+    console.log(sumAll(1,[1,3]))
 
 // Do not edit below this line
 module.exports = sumAll;
