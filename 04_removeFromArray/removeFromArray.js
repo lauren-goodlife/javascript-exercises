ea02f50365825f07af47ff9b0d0c8ca4c59f1c95
Lorena <106349039+lauren-goodlife@users.noreply.github.com>
2024-09-26 22:01:10 +0200

    Exercise resolved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,30 @@
-const removeFromArray = function() {
-};
+function removeFromArray(array, ...valuesToRemove) {
+
+
+    for (let i = array.length - 1; i >= 0; i--) {
+
+
+        for (let j = 0; j < valuesToRemove.length; j++) {
+            
+            
+
+            if (array[i] === valuesToRemove[j]) {
+                array.splice(i, 1)
+            }
+            
+        }
+
+        
+    }
+return array;    
+
+}
+
+console.log(removeFromArray([1,2,"1","hey","ho"],1,2,"hey"))
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
