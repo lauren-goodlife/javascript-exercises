786c6ea6c79010eb288fa80268c41a9794c88383
Lorena <106349039+lauren-goodlife@users.noreply.github.com>
2024-09-28 19:56:15 +0200

    Exercise resolved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,28 @@
-const convertToCelsius = function() {
-};
+function convertToCelsius(tempInFahrenheit){
 
-const convertToFahrenheit = function() {
-};
+  if (tempInFahrenheit === 32) {
+      return 0;
+  }
+
+  var tempInCelsius = ((tempInFahrenheit - 32) * (5/9));
+  var roundedTempInCelsius = Number(tempInCelsius.toFixed(1));
+  return roundedTempInCelsius;
+
+}
+
+
+
+function convertToFahrenheit(tempInCelsius){
+
+  if (tempInCelsius === 0) {
+      return 32;
+  }
+  
+  var tempInFahrenheit = (tempInCelsius * (9/5) + 32)
+  var roundedTempInFahrenheit = Number(tempInFahrenheit.toFixed(1));
+  return roundedTempInFahrenheit;
+  
+  }
 
 // Do not edit below this line
 module.exports = {
