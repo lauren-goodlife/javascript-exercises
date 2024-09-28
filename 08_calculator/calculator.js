7add5c8c6c632dc7948dc0d6afc58702ec1428fb
Lorena <106349039+lauren-goodlife@users.noreply.github.com>
2024-09-28 21:28:13 +0200

    Exercise resolved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,71 @@
-const add = function() {
-	
-};
 
-const subtract = function() {
-	
-};
+let add = (num1, num2) => num1+num2;
 
-const sum = function() {
-	
-};
+let subtract = (num1, num2) => num1 - num2;
 
-const multiply = function() {
+const sum = function(array) {
 
-};
+  if (array.length === 0) {
+    return 0;
+  }
+  
+  if (array.length === 1) {
+    return Number(array[0]);
+  }
 
-const power = function() {
-	
-};
+  if(array.length > 1){
 
-const factorial = function() {
+  let totalSum = 0;
+    for (let i = 0; i < array.length; i++) {
+      totalSum += Number(array[i]);
+    }
+    return totalSum;
+    } 
+  };
+  
+const multiply = function(array) {
+
+      if (array.length === 0) {
+        return 0;
+      }
+      
+      if (array.length === 1) {
+        return Number(array[0]);
+      }
+    
+      if(array.length > 1){
+    
+      let totalMultiplication = 1;
+        for (let i = 0; i < array.length; i++) {
+          totalMultiplication = totalMultiplication * Number(array[i]);
+        }
+        return totalMultiplication;
+        } 
+      };
+
+
+
+const power = function(baseNumber,esponentNumber) {
+    
+      let totalPower = 1;
+        for (let i = 1; i <= esponentNumber; i++) {
+          totalPower = totalPower * baseNumber;
+        }
+        return totalPower;
+        } 
+
+const factorial = function(numberToFactorize) {
+
+if (numberToFactorize == 0 || numberToFactorize == 1) {
+  return 1;  
+}
+
+let totalFactorial = 1;
+for (let i = numberToFactorize; i > 0; i--){
+  totalFactorial = totalFactorial * i;
+}
+return totalFactorial;
+
 	
 };
 
