b1c40a1bc69035c2fa69fa233694f9793e5ed403
Lorena <106349039+lauren-goodlife@users.noreply.github.com>
2024-09-28 22:31:56 +0200

    Exercise resolved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,35 @@
-const palindromes = function () {
+const palindromes = function palindrome(array) {
 
-};
+    let newArray = array
+        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")  
+        .toLowerCase()                              
+        .replace(/\s/g, '')                          
+        .split("");                                  
+    
+    newArray = newArray.map(function(item) {
+      return item.trim();                            
+    });
+    
+        
+    for (let i = 0; i < newArray.length/2; i++) {
+    
+        for (let j = newArray.length - 1 - i; j >= 0 ; j--) {
+    
+            console.log(newArray[i])
+            console.log(newArray[j])
+            if (newArray[i] !== newArray[j]) {
+                return false;
+            }
+    
+            break;
+    
+        }
+    
+    } 
+    return true;
+    
+    
+    };
 
 // Do not edit below this line
 module.exports = palindromes;
